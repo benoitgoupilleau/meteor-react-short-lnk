96ebfc131d3c81bcad914915f309c9a162e93409
benoitgoupilleau <benoit.goupilleau@gmail.com>
2017-05-05 15:44:19 +0200

    Initial commit

---
+++ imports/ui/LinksList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Session} from 'meteor/session';
+import {Links} from '../api/links';
+import LinkListItem from './LinkListItem'
+
+export default class LinksList extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      links: []
+    }
+  }
+  componentDidMount(){
+    this.linksTracker = Tracker.autorun(()=>{
+      Meteor.subscribe('links');
+      let links = Links.find({visible: Session.get('showVisible')}).fetch();
+      this.setState({links});
+    })
+  }
+  componentWillUnmount(){
+    this.linksTracker.stop();
+  }
+  renderLinksListItems(){
+    return this.state.links.map((link)=>{
+      const shortUrl=Meteor.absoluteUrl(link._id)
+      return <LinkListItem key={link._id} shortUrl={shortUrl} {...link}/>
+      // return <p key={link._id}>{link.url}</p>;
+    })
+  }
+  render(){
+    return (
+      <div>
+        <p>Links List</p>
+        <div>
+          {this.renderLinksListItems()}
+        </div>
+      </div>
+    )
+  }
+}
